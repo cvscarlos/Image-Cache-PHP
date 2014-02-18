6e1cf978c59e685fb6805de6d2718b3bda99d765
Carlos Vinicius <carlosjp@hotmail.co.jp>
2014-02-18 09:49:03 -0300

    Ajusta indentação para tabulações

--- ImageCache.class.php
+++ ImageCache.class.php
@@ -16,159 +16,159 @@
 
 class ImageCache
 {
-    private $url;
-    private $outUrl;
-    private $width;
-    private $height;
-    private $imageGd2;
-    private $crop;
-    private $cacheDir;
-    private $cacheUrl;
-    private $newFileName;
-    private $resolution;
-    
-    function __construct($cacheDir,$cacheUrl)
-    {
-        $this->cacheDir=$cacheDir;
-        $this->cacheUrl=$cacheUrl;
-    }
-    
-    public function printUrl($url,$resolution=72,$width=1600,$height=1600,$crop=false)
-    {
-        $this->crop=$crop;
-        echo $this->getUrl($url,$resolution,$width,$height,$crop);
-    }
+	private $url;
+	private $outUrl;
+	private $width;
+	private $height;
+	private $imageGd2;
+	private $crop;
+	private $cacheDir;
+	private $cacheUrl;
+	private $newFileName;
+	private $resolution;
+	
+	function __construct($cacheDir,$cacheUrl)
+	{
+		$this->cacheDir=$cacheDir;
+		$this->cacheUrl=$cacheUrl;
+	}
+	
+	public function printUrl($url,$resolution=72,$width=1600,$height=1600,$crop=false)
+	{
+		$this->crop=$crop;
+		echo $this->getUrl($url,$resolution,$width,$height,$crop);
+	}
 	
 	public function getImgData()
 	{
 		return getimagesize($this->cacheDir."/".$this->newFileName);
 	}
-    
-    public function getUrl($url,$resolution=72,$width=1600,$height=1600,$crop=false)
-    {
-        $this->resolution=$resolution;
-        $this->newFileName=sha1($url.$resolution.$width.$height.$crop).".jpg";
-        $this->outUrl=$this->cacheUrl."/".$this->newFileName;
-        if(!file_exists($this->cacheDir."/".$this->newFileName))
-        {
-            $this->crop=$crop;
-            $this->width=$width;
-            $this->height=$height;
-            $this->url=$url;
-            $this->resize();
-        }
-        return $this->outUrl;
-    }
-    
-    private function resize()
-    {
-        if($this->crop)
-            $this->resizeCrop();
-        else
-            $this->resizeProportionally();  
-        $this->saveImage();
-    }
-    
-    private function resizeProportionally()
-    {
-        list($width_orig, $height_orig, $imageType) = getimagesize($this->url);
-        $ratio_orig = $width_orig/$height_orig;
+	
+	public function getUrl($url,$resolution=72,$width=1600,$height=1600,$crop=false)
+	{
+		$this->resolution=$resolution;
+		$this->newFileName=sha1($url.$resolution.$width.$height.$crop).".jpg";
+		$this->outUrl=$this->cacheUrl."/".$this->newFileName;
+		if(!file_exists($this->cacheDir."/".$this->newFileName))
+		{
+			$this->crop=$crop;
+			$this->width=$width;
+			$this->height=$height;
+			$this->url=$url;
+			$this->resize();
+		}
+		return $this->outUrl;
+	}
+	
+	private function resize()
+	{
+		if($this->crop)
+			$this->resizeCrop();
+		else
+			$this->resizeProportionally();  
+		$this->saveImage();
+	}
+	
+	private function resizeProportionally()
+	{
+		list($width_orig, $height_orig, $imageType) = getimagesize($this->url);
+		$ratio_orig = $width_orig/$height_orig;
 
-        if($this->width<$width_orig || $this->height<$height_orig)
-        {
-            if ($this->width/$this->height > $ratio_orig)
-               $this->width = $this->height*$ratio_orig;
-            else
-               $this->height = $this->width/$ratio_orig;
-        }
-        else
-        {
-            $this->width=$width_orig;
-            $this->height=$height_orig;
-        }
+		if($this->width<$width_orig || $this->height<$height_orig)
+		{
+			if ($this->width/$this->height > $ratio_orig)
+			   $this->width = $this->height*$ratio_orig;
+			else
+			   $this->height = $this->width/$ratio_orig;
+		}
+		else
+		{
+			$this->width=$width_orig;
+			$this->height=$height_orig;
+		}
 
-        $image_p = imagecreatetruecolor($this->width, $this->height);
-        switch($imageType)
-        {
-            case 1:
-                $image = imagecreatefromgif($this->url);
-                break;
-            case 2:
-                $image = imagecreatefromjpeg($this->url);
-                break;
-            case 3:
-                $image = imagecreatefrompng($this->url);
-                break;
-        }
-        imagecopyresampled($image_p, $image, 0, 0, 0, 0, $this->width, $this->height, $width_orig, $height_orig);
-        $this->imageGd2=$image_p;
-    }
-    
-    private function resizeCrop()
-    {
-        $width_crop=$this->width;
-        $height_crop=$this->height;
-        list($width_orig, $height_orig, $imageType) = getimagesize($this->url);
-        $tmpSize=($width_orig>$height_orig?$width_orig:$height_orig)*2; //para que W ou H (o que tiver o menor valor) seja igual a dimensão definida para o quadrado ($width_crop)
-        
-        if($width_orig>$height_orig)
-        {
-            $width=$tmpSize;
-            $height=$width_crop;
-        }
-        elseif($width_orig==$height_orig)
-        {
-            $width=$width_crop;
-            $height=$width_crop;
-        }
-        else
-        {
-            $width=$width_crop;
-            $height=$tmpSize;
-        }
+		$image_p = imagecreatetruecolor($this->width, $this->height);
+		switch($imageType)
+		{
+			case 1:
+				$image = imagecreatefromgif($this->url);
+				break;
+			case 2:
+				$image = imagecreatefromjpeg($this->url);
+				break;
+			case 3:
+				$image = imagecreatefrompng($this->url);
+				break;
+		}
+		imagecopyresampled($image_p, $image, 0, 0, 0, 0, $this->width, $this->height, $width_orig, $height_orig);
+		$this->imageGd2=$image_p;
+	}
+	
+	private function resizeCrop()
+	{
+		$width_crop=$this->width;
+		$height_crop=$this->height;
+		list($width_orig, $height_orig, $imageType) = getimagesize($this->url);
+		$tmpSize=($width_orig>$height_orig?$width_orig:$height_orig)*2; //para que W ou H (o que tiver o menor valor) seja igual a dimensão definida para o quadrado ($width_crop)
+		
+		if($width_orig>$height_orig)
+		{
+			$width=$tmpSize;
+			$height=$width_crop;
+		}
+		elseif($width_orig==$height_orig)
+		{
+			$width=$width_crop;
+			$height=$width_crop;
+		}
+		else
+		{
+			$width=$width_crop;
+			$height=$tmpSize;
+		}
 
-        $ratio_orig=$width_orig/$height_orig;
+		$ratio_orig=$width_orig/$height_orig;
 
-        if ($width/$height > $ratio_orig)
-           $width=$height*$ratio_orig;
-        else
-           $height=$width/$ratio_orig;
+		if ($width/$height > $ratio_orig)
+		   $width=$height*$ratio_orig;
+		else
+		   $height=$width/$ratio_orig;
 
-        $image_p=imagecreatetruecolor($width_crop, $height_crop);
-        switch($imageType)
-        {
-            case 1:
-                $image = imagecreatefromgif($this->url);
-                break;
-            case 2:
-                $image = imagecreatefromjpeg($this->url);
-                break;
-            case 3:
-                $image = imagecreatefrompng($this->url);
-                break;
-        }
+		$image_p=imagecreatetruecolor($width_crop, $height_crop);
+		switch($imageType)
+		{
+			case 1:
+				$image = imagecreatefromgif($this->url);
+				break;
+			case 2:
+				$image = imagecreatefromjpeg($this->url);
+				break;
+			case 3:
+				$image = imagecreatefrompng($this->url);
+				break;
+		}
 
-        if($width_orig>$height_orig)
-        {
-            $width_crop_end=-($width-$height)/2;
-            $height_crop_end=0;
-        }
-        elseif($width_orig==$height_orig)
-        {
-            $width_crop_end=0;
-            $height_crop_end=0;
-        }
-        else
-        {
-            $width_crop_end=0;
-            $height_crop_end=-($height-$width)/2;
-        }
-        imagecopyresampled($image_p, $image, $width_crop_end, $height_crop_end, 0, 0, $width, $height, $width_orig, $height_orig);
-        $this->imageGd2=$image_p;
-    }
-    
-    private function saveImage()
-    {
-        imagejpeg($this->imageGd2, $this->cacheDir."/".$this->newFileName, $this->resolution);
-    }
+		if($width_orig>$height_orig)
+		{
+			$width_crop_end=-($width-$height)/2;
+			$height_crop_end=0;
+		}
+		elseif($width_orig==$height_orig)
+		{
+			$width_crop_end=0;
+			$height_crop_end=0;
+		}
+		else
+		{
+			$width_crop_end=0;
+			$height_crop_end=-($height-$width)/2;
+		}
+		imagecopyresampled($image_p, $image, $width_crop_end, $height_crop_end, 0, 0, $width, $height, $width_orig, $height_orig);
+		$this->imageGd2=$image_p;
+	}
+	
+	private function saveImage()
+	{
+		imagejpeg($this->imageGd2, $this->cacheDir."/".$this->newFileName, $this->resolution);
+	}
 }
